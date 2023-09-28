1379867d8085d93c3d9e8523313c6c20c3322575
Vasiliy Myagotin <76815748+Lepert1@users.noreply.github.com>
2023-09-28 17:44:39 +0300

    Update UserDaoHibernateImpl.java
    Добавил rollback в случае, если возникнет исключение

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,13 +3,17 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
+import static org.hibernate.resource.transaction.spi.TransactionStatus.ACTIVE;
+
 public class UserDaoHibernateImpl implements UserDao {
     Configuration configuration = new Configuration().addAnnotatedClass(User.class);
     SessionFactory sessionFactory = configuration.buildSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
@@ -17,44 +21,71 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
+            transaction = session.getTransaction();
+            transaction.begin();
             String sqlQuery = "CREATE TABLE IF NOT EXISTS Users (id serial PRIMARY KEY, name varchar(255), lastname varchar(255), age smallint);";
-            session.beginTransaction();
             session.createSQLQuery(sqlQuery).executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction.getStatus() == ACTIVE) {
+                transaction.rollback();
+            }
         }
-
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
         String sqlQuery = "DROP TABLE IF EXISTS users";
         try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
+            transaction = session.getTransaction();
+            transaction.begin();
             session.createSQLQuery(sqlQuery).executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction.getStatus() == ACTIVE) {
+                transaction.rollback();
+            }
         }
+
+
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
+            transaction = session.getTransaction();
+            transaction.begin();
             User user = new User(name, lastName, age);
             session.save(user);
-            session.getTransaction().commit();
-            session.close();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction.getStatus() == ACTIVE) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
+            transaction = session.getTransaction();
+            transaction.begin();
             User user = session.get(User.class, id);
             if (user != null) session.delete(user);
-            session.getTransaction().commit();
-            session.close();
+            transaction.commit();
+        }catch (Exception e) {
+            e.printStackTrace();
+            if (transaction.getStatus() == ACTIVE) {
+                transaction.rollback();
+            }
         }
     }
 
